0fb9dd6b977ba1c5f6d131fd3054fd697b885d20
Ashav1309 <ashav1309@gmail.com>
2025-01-09 02:38:20 +0500

    com

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Credit Default",
+    name="MLOPS_3_Lab1",
     version="0.1",
     description="Credit default analysis package",
     author="Alexandr Shavaliev",
